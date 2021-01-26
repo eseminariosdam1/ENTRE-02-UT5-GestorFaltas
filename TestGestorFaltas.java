183cd74777cbd1ccdadb997b46b60c319d40087f
Eneko Seminario <eseminariosdam1@gmail.com>
2021-01-26 20:19:33 +0100

    Proyecto Terminado

--- TestGestorFaltas.java
+++ TestGestorFaltas.java
@@ -10,12 +10,15 @@ public class TestGestorFaltas {
     public static void main(String[] args) {
             GestorFaltas curso = new GestorFaltas(Integer.parseInt(args[0]));
             
+            // Muestra los alumnos/as del curso
             curso.leerDeFichero();
             System.out.println(curso.toString());
             
+            // Muestra las faltas que se le han justificado al alumno/a
             curso.justificarFaltas("IRISO FLAMARIQUE", 6);
             System.out.println("Se le han justificado 6 faltas a IRISO FLAMARIQUE, Carla ");
             
+            // Ordena de ayor a menor el numero de faltas injustificadas
             curso.ordenar();
             System.out.println("Se han ordenado de mayor a menor numero de faltas injustificadas");
             
